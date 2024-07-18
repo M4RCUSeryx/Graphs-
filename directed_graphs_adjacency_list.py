492ea193846a7b732de5a7f1ecba85f749d234e0
Satyajeet Kumar <97826624+M4RCUSeryx@users.noreply.github.com>
2024-07-19 04:26:52 +0530

    Update directed_graphs_adjacency_list.py

--- directed_graphs_adjacency_list.py
+++ directed_graphs_adjacency_list.py
@@ -1,3 +1,5 @@
+dedges = [(0,1,10),(0,2,80),(1,2,6),(1,4,20),(2,3,70),(4,5,50),(4,6,5),(5,6,10)] #sample edges of a directed graph
+size = n #n be the number of vertices in graph
 WL = {}
 for i in range(size):
   WL[i] = []
