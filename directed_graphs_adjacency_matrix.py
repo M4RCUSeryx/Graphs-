2f8cf8578c3eb9e4d914c402d8b6dc759a47b041
Satyajeet Kumar <97826624+M4RCUSeryx@users.noreply.github.com>
2024-07-19 03:56:27 +0530

    Update directed_graphs_adjacency_matrix.py

--- directed_graphs_adjacency_matrix.py
+++ directed_graphs_adjacency_matrix.py
@@ -1,8 +1,9 @@
-dedges = [(0,1,10),(0,2,80),(1,2,6),(1,4,20),(2,3,70),(4,5,50),(4,6,5),(5,6,10)]
-size = 7
+dedges = [(0,1,10),(0,2,80),(1,2,6),(1,4,20),(2,3,70),(4,5,50),(4,6,5),(5,6,10)] #sample edges of a directed graph
+size = n
 import numpy as np
-W = np.zeros(shape=(size,size,2))
+W = np.zeros(shape=(n,n,2))
 for (i,j,d) in dedges:
-  W[i,j,0] = 1
+  # updates the values as per the edges and their weights in the graphs
+  W[i,j,0] = 1 # W[i,j,0] = 1 if the (i,j) edge exists in graph
   W[i,j,1] = d
 print(W)
