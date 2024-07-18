64fc396135a867a0b0402da0682712dff43ab5e8
Satyajeet Kumar <97826624+M4RCUSeryx@users.noreply.github.com>
2024-07-19 04:30:57 +0530

    Create undirected_graphs_adjacency_list.py

---
+++ undirected_graphs_adjacency_list.py
@@ -0,0 +1,9 @@
+dedges = [(0,1,10),(0,2,80),(1,2,6),(1,4,20),(2,3,70),(4,5,50),(4,6,5),(5,6,10)] #sample edges of a directed graph
+size = n #n be the number of vertices in graph
+edges = dedges + [(j,i,d) for (i,j,d) in dedges]
+WL = {}
+for i in range(size):
+  WL[i] = []
+for (i,j,d) in edges:
+  WL[i].append((j,d))
+print(WL)
