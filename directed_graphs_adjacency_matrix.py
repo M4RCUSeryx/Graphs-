f6b4bab461b8c1ef0cb5b6f7193450621b1e03b6
Satyajeet Kumar <97826624+M4RCUSeryx@users.noreply.github.com>
2024-07-19 04:26:46 +0530

    Update directed_graphs_adjacency_matrix.py

--- directed_graphs_adjacency_matrix.py
+++ directed_graphs_adjacency_matrix.py
@@ -1,5 +1,5 @@
 dedges = [(0,1,10),(0,2,80),(1,2,6),(1,4,20),(2,3,70),(4,5,50),(4,6,5),(5,6,10)] #sample edges of a directed graph
-size = n
+size = n #n be the number of vertices in graph
 import numpy as np
 W = np.zeros(shape=(n,n,2))
 for (i,j,d) in dedges:
